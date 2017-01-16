22159e505dd8d3f0095c0ccd4611e12de819df4e
dhartaros <frankvaldez@outlook.com>
2017-01-16 00:23:18 -0700

    Habilidades puestas en columnas

--- assets/js/more.js
+++ assets/js/more.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
-	var skills = $("#skills");
+	var skills = $("#skill_list");
 	var info = JSON.parse(skillsJson);
+
 	for(var i in info) {
 		var skill = "<p>" + info[i].tool + "</p>";
+		skills.append(skill);
+	}
+
+	for(var i in info) {
 		var level = info[i].level;
 		var aux = "<p>";
 		
 		for (var j = 1; j <= level; j++) {
-			aux += "<span class='icon fa-star'>";
+			aux += "<span class='icon fa-star'></span>";
 		}
 
 		for (j; j <= 5; j++) {
-			aux += "<span class='icon fa-star-o'>";
+			aux += "<span class='icon fa-star-o'></span>";
 		}
-		skill += aux + "</span></p><br>"
-		skills.append(skill);
+		aux += "</p>"
+		skills.append(aux);
 	}
+
 	var projects = $("#work");
 	info = JSON.parse(workJson);
+
 	for(var i in info) {
 		var title = "";
 		var image = "";
