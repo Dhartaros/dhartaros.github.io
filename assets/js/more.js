e4e11400ca00351d6b2f9ab76504a7c6b8af4c76
dhartaros <frankvaldez@outlook.com>
2017-01-15 23:23:07 -0700

    Archivos minificados y se pasaron los proyectos realizados a un JSON

--- assets/js/more.js
+++ assets/js/more.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var skills = $("#skills");
-	var info = JSON.parse(data);
+	var info = JSON.parse(skillsJson);
 	for(var i in info) {
 		var skill = "<p>" + info[i].tool + "</p>";
 		var level = info[i].level;
@@ -15,5 +15,20 @@ $(document).ready(function() {
 		}
 		skill += aux + "</span></p><br>"
 		skills.append(skill);
+	}
+	var projects = $("#work");
+	info = JSON.parse(workJson);
+	for(var i in info) {
+		var title = "";
+		var image = "";
+		if (info[i].url === undefined)
+			title += "<h3>" + info[i].name + "</h3>";
+		else
+			title += "<h3><a href='" + info[i].url + "' target='_blank'>Java Sensei</a></h3>";
+		if (info[i].imageUrl !== undefined)
+			image += "<span class='image main'><img src='" + info[i].imageUrl + "' alt='" + info[i].name + "Image' /></span>";
+		var description = "<p>" + info[i].description + "</p>";
+		var project = title + image + description;
+		projects.append(project);
 	}
 });
